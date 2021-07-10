1808206785596294ddfe93f1110bf00ba28cb509
Arun <sarun6153@gmail.com>
2021-07-10 22:56:49 +0530

    cursor renewed

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as constants from '../../_shared/constants';
 
 @Component({
@@ -10,10 +10,43 @@ export class LandingPageComponent implements OnInit {
 
   public constants = constants;
 
-  constructor() { }
+  @ViewChild("cursor", { read: ElementRef }) cursor: ElementRef;
+  public tempSelector:any;
+  constructor(elem:ElementRef) { }
 
   ngOnInit() {
 
+  }
+
+  public ngAfterViewInit ():void
+  {
+    this.tempSelector = this.cursor.nativeElement;
+    var timeout;
+
+    //follow cursor on mousemove
+    document.addEventListener("mousemove", (e) => {
+      let x = e.pageX;
+      let y = e.pageY;
+
+      this.tempSelector.style.top = y + "px";
+      this.tempSelector.style.left = x + "px";
+      this.tempSelector.style.display = "block";
+
+      //cursor effects when mouse stopped
+      const mouseStopped = ()=>{
+        if(this.tempSelector)
+          this.tempSelector.style.display = "none";
+      };
+
+      clearTimeout(timeout);
+      timeout = setTimeout(mouseStopped, 1000);
+    });
+
+    //cursor effects when mouseout
+    document.addEventListener("mouseout", () => {
+      if(this.tempSelector)
+      this.tempSelector.style.display = "none";
+    });
   }
 
 }
