97a4dbbc3809137fd16ea2d8f1b55fcb17d3feb3
Arun Saini <sarun6153@gmail.com>
2021-10-13 02:13:44 +0530

    Changes due to new resume

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as constants from '../../_shared/constants';
-import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
+// import jsPDF from 'jspdf';
+// import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-landing-page',
@@ -83,13 +83,20 @@ export class LandingPageComponent implements OnInit {
   }
 
   public downloadAsPDF() {
-    let DATA = document.getElementById('pdfResume');
+    let link = document.createElement('a');
+    link.setAttribute('type', 'hidden');
+    link.href = 'assets/Arun Saini-CV_10-13-2021.pdf';
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    // let DATA = document.getElementById('pdfResume');
 
-    html2canvas(DATA).then(canvas => {
-        const FILEURI = canvas.toDataURL('image/png')
-        let PDF = new jsPDF('p','pt', [canvas.width+10, canvas.height]);
-        PDF.addImage(FILEURI, 'JPEG', 0, 0, canvas.width, canvas.height)
-        PDF.save('Arun Saini - CV.pdf');
-    });
+    // html2canvas(DATA).then(canvas => {
+    //     const FILEURI = canvas.toDataURL('image/png')
+    //     let PDF = new jsPDF('p','pt', [canvas.width+10, canvas.height]);
+    //     PDF.addImage(FILEURI, 'JPEG', 0, 0, canvas.width, canvas.height)
+    //     PDF.save('Arun Saini - CV.pdf');
+    // });
+
   }
 }
